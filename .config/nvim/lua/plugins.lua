79506777915a8d3f9db0aad3c1faf3c8ff9b5cea
F34DB3R <jellywalkeryt@gmail.com>
2025-09-17 12:41:41 +0300

    init

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,24 @@
+return require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'morhetz/gruvbox'
+	use 'junegunn/fzf'
+	use 'junegunn/fzf.vim'
+	-- use { 'neoclide/coc.nvim', branch = 'release' }
+	use 'rust-lang/rust.vim'
+	use {
+  	'nvim-lualine/lualine.nvim',
+  	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'christoomey/vim-tmux-navigator'
+        use {
+                'vyfor/cord.nvim',
+                run = ':Cord update',
+                -- config = function()
+                --   require('cord').setup {}
+                -- end
+        }
+end)
